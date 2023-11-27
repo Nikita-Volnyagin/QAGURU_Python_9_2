74b732826acaefb4ea4181d007ae9cb6078852a3
Nikita <nikita.volnyagin@yandex.ru>
2023-11-27 14:35:44 +0400

    Добавили .idea

---
+++ test_fixtures.py
@@ -0,0 +1,20 @@
+import pytest
+
+@pytest.fixture
+def browser():
+    print("я выполняюсь перед тестом. Открываем браузер!")
+
+    yield "Browser"
+
+    print("Я выполняюсь после теста. Закрываем браузер!")
+
+
+@pytest.fixture
+def login_page(browser):
+    pass
+
+@pytest.fixture
+def credentials():
+    return "admin", "12345"
+def test_login(login_page, credentials):
+    assert credentials == ("admin", "12345"), "Неверный логин и пароль админа"
