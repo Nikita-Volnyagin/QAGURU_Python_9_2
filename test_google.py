07a5d029e0989a5efc2834f3aee3ae5ca10fcef8
Nikita <nikita.volnyagin@yandex.ru>
2023-11-27 15:38:02 +0400

    Внёс правки, по замечаниям.

--- test_google.py
+++ test_google.py
@@ -1,10 +1,12 @@
 from selene.support.shared import browser
 from selene import be, have
 
+
 def test_google_search():
     browser.open('https://www.google.ru/')
     browser.element('#APjFqb').should(be.blank).type('yashaka/selen').press_enter()
     browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_google_search_not_found():
     browser.open('https://www.google.ru/')
